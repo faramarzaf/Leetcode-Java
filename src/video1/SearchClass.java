c9c42f42d6a00242dbb9b5f5badb96f90bb39196
faramarzaf <faramarzafzali@gmail.com>
2022-08-06 16:05:54 +0430

    update 06Aug2022 4:05pm added binary search

--- src/video1/SearchClass.java
+++ src/video1/SearchClass.java
@@ -1,7 +1,5 @@
 package video1;
 
-import java.util.Arrays;
-
 public class SearchClass {
 
     public static void main(String[] args) {
@@ -9,7 +7,12 @@ public class SearchClass {
        /* System.out.println(atEvenIndex("sorem", 's'));
         System.out.println(atEvenIndex("myname", 'y'));
         System.out.println(atEvenIndex("heSSR", 'R'));*/
-        System.out.println(linearSearch(new int[]{1, 2, 3}, 3));
+        // System.out.println(linearSearch(new int[]{1, 2, 3}, 3));
+
+
+        System.out.println(binarySearch(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 9));
+        System.out.println(binarySearch2(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1}, 9));
+
     }
 
     private static boolean atEvenIndex(String s, char c) {
@@ -32,8 +35,39 @@ public class SearchClass {
         return false;
     }
 
-    // usually the input data is sorted
-    private boolean binarySearch(int[] arr, int item) {
-       return false;
+    private static boolean binarySearch(int[] arr, int item) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+
+            if (arr[mid] == item) return true;
+
+            else if (item < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    private static boolean binarySearch2(int[] arr, int item) {// desc order of input
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (right - left) / 2;
+
+            if (item == arr[mid]) return true;
+            else if (item < arr[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
     }
 }
